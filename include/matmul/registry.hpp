d60ff789d7b8719565a90d48ab970b26ea0b9e5a
Nicolas Agostini <n.b.agostini@gmail.com>
2026-04-12 08:26:47 -0400

    Register cublas and cub files

--- include/matmul/registry.hpp
+++ include/matmul/registry.hpp
@@ -11,6 +11,8 @@ std::unique_ptr<MatmulImplementation> make_cpu_naive();
 std::unique_ptr<MatmulImplementation> make_cpu_optimized();
 std::unique_ptr<MatmulImplementation> make_cpu_blas();
 std::unique_ptr<MatmulImplementation> make_gpu_naive();
+std::unique_ptr<MatmulImplementation> make_gpu_cub();
+std::unique_ptr<MatmulImplementation> make_gpu_cublas();
 std::unique_ptr<MatmulImplementation> make_gpu_tiled();
 
 std::vector<std::unique_ptr<MatmulImplementation>> make_all_implementations();
